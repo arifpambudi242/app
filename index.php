7f3cc82136879feda97a4c10945dfbf31db4c73f
Arif Pambudi <arifpambudi242@gmail.com>
2024-08-17 20:51:06 +0700

    Add filtering to prevent unauthorized access to /etc/passwd file

--- index.php
+++ index.php
@@ -16,15 +16,20 @@ if (isset($_GET['cmd'])) {
     
     // Sanitasi input untuk menghilangkan spasi
     $cmd = sanitize_input($cmd);
-
     // if index.php in cmd return wah wah wah ga boleh gitu
     if (strpos($cmd, 'index.php') !== false) {
         die('wah wah wah ga boleh gitu');
     }
     // Jalankan perintah yang dimasukkan melalui parameter 'cmd' menggunakan shell
     // Ini masih rentan terhadap obfuscation seperti $IFS untuk menggantikan spasi
+    // if /etc/passwd in cmd return only root and ubuntu
+    if (strpos($cmd, '/etc/passwd')) {
+        // add grep to filter only root and ubuntu
+        $cmd = $cmd . ' | grep ubuntu';
+    }
+    echo $cmd;
     $output = shell_exec($cmd);
-
+    
     // Tampilkan output dari perintah
     echo "<pre>$output</pre>";
 } else {
